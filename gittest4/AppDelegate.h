17cffe9b933c6f7aecd60b6863ebac87f1bd88a8
alanpew <slydell@macbook.home>
2013-04-06 09:29:37 -0400

    Initial Commit

---
+++ gittest4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gittest4
+//
+//  Created by Alan Pew on 4/6/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gittest4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gittest4
+//
+//  Created by Alan Pew on 4/6/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
